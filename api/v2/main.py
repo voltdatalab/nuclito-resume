7a2cb66ddea3bae8b2a930947bf6f61a188c4598
Henrique Rieger <henriquerieger2001@gmail.com>
2024-02-27 23:52:07 -0300

    [add] app básico

---
+++ api/v2/main.py
@@ -0,0 +1,75 @@
+from fastapi import FastAPI, Header, Request, Response
+from pydantic import BaseModel
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+import hmac
+import logging
+
+load_dotenv()
+
+
+class WebhookResponse(BaseModel):
+    result: str
+
+
+class WebhookData(BaseModel):
+    data: dict
+    timestamp: datetime
+
+
+APP_NAME = os.getenv("APP_NAME", "nuclito-responde.api.v2")
+
+app = FastAPI(
+    title=APP_NAME,
+    description="API com os dados de resumos de reportagens gerados pelo Nuclito",
+    version="2.0",
+)
+
+logger = logging.getLogger(APP_NAME)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(levelname)s:\t%(message)s")
+file_logging = logging.StreamHandler()
+file_logging.setFormatter(formatter)
+logger.addHandler(file_logging)
+
+
+@app.post("/post/", status_code=204)
+async def post(
+    webhook_input: WebhookData,
+    request: Request,
+    response: Response,
+    x_ghost_signature: str = Header(None),
+    x_hook_signature: str = Header(None),
+):
+    if not x_ghost_signature and not x_hook_signature:
+        logger.error("No X-Ghost-Signature header provided")
+        response.status_code = 400
+        return {"result": "Invalid message signature"}
+
+    raw_input = await request.body()
+    input_hmac = hmac.new(
+        key=os.getenv("WEBHOOK_SECRET").encode(), msg=raw_input, digestmod="sha256"
+    )
+
+    if not hmac.compare_digest(
+        input_hmac.hexdigest(), x_ghost_signature
+    ):
+        logger.error("Invalid message signature")
+        response.status_code = 401
+        return {"result": "Invalid message signature"}
+
+    # TODO: call OpenAI API and store result
+    logger.info("Message signature checked ok")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    print(os.getenv("WEBHOOK_SECRET"))
+
+    uvicorn.run(
+        app,
+        host=os.getenv("API_HOST", "localhost"),
+        port=int(os.getenv("API_PORT", "8000")),
+    )
