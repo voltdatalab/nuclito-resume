f7ca6721c023d16ea130a11ed62f3334eb37d098
Henrique Rieger <henriquerieger2001@gmail.com>
2024-03-03 17:42:57 -0300

    [add] link no post

--- api/v2/database/migrations/posts.py
+++ api/v2/database/migrations/posts.py
@@ -9,6 +9,7 @@ class Post(Migration):
     __tablename__ = "posts"
     id: Mapped[str] = mapped_column(String(32), primary_key=True)
     title: Mapped[str] = mapped_column(Text(), nullable=True)
+    link: Mapped[str] = mapped_column(String(64))
     summary: Mapped[str] = mapped_column(Text())
     timestamp: Mapped[datetime] = mapped_column(DateTime(), default=datetime.now())
 
@@ -19,13 +20,13 @@ class Post(Migration):
         return str(self.summary)
 
     def __repr__(self) -> str:
-        return f"Post(id={self.id}, title={self.title}, summary={self.summary_str()}, timestamp={self.timestamp})"
+        return f"Post(id={self.id}, title={self.title}, link={self.link}, summary={self.summary_str()}, timestamp={self.timestamp})"
 
 
 if __name__ == "__main__":
     Post.run()
     post = Post(
-        id="teste123", title="Teste de Título", summary="<html>Um teste de corpo</html>"
+        id="teste123", title="Teste de Título", link="https://nucleo.jor.br/garimpo/teste123",  summary="<html>Um teste de corpo</html>"
     ).insert()
 
     for p in Post.select():
