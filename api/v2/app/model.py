331f56d465bd3bb5244eb4e28e24de6b0c09ecf9
Henrique Rieger <henriquerieger2001@gmail.com>
2024-03-14 00:06:03 -0300

    [fix] sessões abertas na função read e pool_size da engine

--- api/v2/app/model.py
+++ api/v2/app/model.py
@@ -17,7 +17,12 @@ url_object = URL.create(
     database=os.getenv("DB_DATABASE"),
 )
 
-engine = create_engine(url_object, echo=True)
+pool_size = int(os.getenv("DB_POOL_SIZE", 20))
+max_overflow = int(os.getenv("DB_MAX_OVERFLOW", 0))
+
+engine = create_engine(
+    url_object, echo=True, pool_size=pool_size, max_overflow=max_overflow
+)
 
 
 class Model(DeclarativeBase):
@@ -44,4 +49,7 @@ class Model(DeclarativeBase):
             result = result.order_by(orderby)
         if limit is not None:
             result = result.limit(limit)
-        return Session(engine).scalars(result)
+
+        with Session(engine) as session:
+            result = list(session.scalars(result))
+        return result
